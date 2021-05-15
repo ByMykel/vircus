98153c17347a45c640624be624a08ff05c5bcdff
ByMykel <bymykel5@gmail.com>
2021-05-15 20:14:27 +0200

    feat: change scrollbar style.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Home from './views/Home.vue';
+import Home from "./views/Home.vue";
 
 export default {
     name: "App",
@@ -26,5 +26,31 @@ export default {
 
 body {
     background: #121212;
+}
+
+::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    -webkit-border-radius: 6px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    -webkit-border-radius: 6px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+    border-radius: 6px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+    background: #b3b3b3;
+}
+
+::-webkit-scrollbar-thumb:window-inactive {
+    background: #b3b3b3;
 }
 </style>
